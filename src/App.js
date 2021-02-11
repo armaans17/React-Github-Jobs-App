7fac6e0cffd64325f2e13015c33ca3942f1cc9f0
Armaan Singh <armaanmaani@gmail.com>
2021-02-11 13:27:17 -0500

    Setup and cleanup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import useFetchJobs from "./useFetchJobs";
+
+export default function App() {
+  const { jobs, loading, error } = useFetchJobs();
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try again</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
+  );
+}
